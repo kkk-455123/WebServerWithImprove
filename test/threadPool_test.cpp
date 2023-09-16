4fe7ee3c6b7fc93c73f5dfbf82dc54e44e777b8e
kkk <614132705@qq.com>
2023-09-16 22:12:29 +0800

    update threadpool with define control

--- test/threadPool_test.cpp
+++ test/threadPool_test.cpp
@@ -3,8 +3,10 @@
 #include <chrono>
 #include "../code/pool/threadpool.h" // 包含你的线程池头文件
 
+// #define TaskType 
+
 // 定义一个任务，这个任务可以是线程池需要执行的具体工作
-void MyTask() {
+void CaculTask() {
     // 这里可以放入具体的任务逻辑
     // 例如，模拟一个简单的计算任务
     volatile int result = 0;
@@ -16,12 +18,12 @@ void MyTask() {
 // 模拟IO任务
 void IOTask() {
     // 模拟IO操作，休眠一段时间
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 // 函数模板
 template<typename T>
-void IO_ThreadPollTest(int nums, T&& task, benchmark::State& state, int tasksNum = 1000) {
+void ThreadPollTest(int nums, T&& task, benchmark::State& state, int tasksNum = 100) {
     // 初始化线程池，使用默认线程数量（8）
     ThreadPool pool(nums);
 
@@ -34,30 +36,61 @@ void IO_ThreadPollTest(int nums, T&& task, benchmark::State& state, int tasksNum
     }
 }
 
+
+#ifdef TaskType
 static void BM_IO_ThreadPoll(benchmark::State& state) {
-    IO_ThreadPollTest(1, IOTask, state);
+    ThreadPollTest(1, IOTask, state);
 }
 // Register the function as a benchmark
 BENCHMARK(BM_IO_ThreadPoll);
 
 static void BM_IO_ThreadPoll2(benchmark::State& state) {
-    IO_ThreadPollTest(2, IOTask, state);
+    ThreadPollTest(2, IOTask, state);
 }
-// BENCHMARK(BM_IO_ThreadPoll2);
+BENCHMARK(BM_IO_ThreadPoll2);
 
 static void BM_IO_ThreadPoll3(benchmark::State& state) {
-    IO_ThreadPollTest(3, IOTask, state);
+    ThreadPollTest(3, IOTask, state);
 }
 // BENCHMARK(BM_IO_ThreadPoll3);
 
 static void BM_IO_ThreadPoll4(benchmark::State& state) {
-    IO_ThreadPollTest(4, IOTask, state);
+    ThreadPollTest(4, IOTask, state);
 }
 // BENCHMARK(BM_IO_ThreadPoll4);
 
 static void BM_IO_ThreadPoll8(benchmark::State& state) {
-    IO_ThreadPollTest(8, IOTask, state);
+    ThreadPollTest(8, IOTask, state);
 }
 // BENCHMARK(BM_IO_ThreadPoll8);
+
+#else
+static void BM_CPU_ThreadPoll(benchmark::State& state) {
+    ThreadPollTest(1, CaculTask, state);
+}
+// Register the function as a benchmark
+BENCHMARK(BM_CPU_ThreadPoll);
+
+static void BM_CPU_ThreadPoll2(benchmark::State& state) {
+    ThreadPollTest(2, CaculTask, state);
+}
+BENCHMARK(BM_CPU_ThreadPoll2);
+
+static void BM_CPU_ThreadPoll3(benchmark::State& state) {
+    ThreadPollTest(3, CaculTask, state);
+}
+BENCHMARK(BM_CPU_ThreadPoll3);
+
+static void BM_CPU_ThreadPoll4(benchmark::State& state) {
+    ThreadPollTest(4, CaculTask, state);
+}
+// BENCHMARK(BM_CPU_ThreadPoll4);
+
+static void BM_CPU_ThreadPoll8(benchmark::State& state) {
+    ThreadPollTest(8, CaculTask, state);
+}
+// BENCHMARK(BM_CPU_ThreadPoll8);
+
+#endif
 
 BENCHMARK_MAIN();
